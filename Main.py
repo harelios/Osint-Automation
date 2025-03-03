2b09a3c5d60dd46a74c8823af84dc189c01ef830
harelios <harel.zenou@gmail.com>
2025-03-03 16:42:28 +0200

    Fix of the programs and the buttons.

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ from tkinter import simpledialog
 
 root = Tk()
 root.title("Automation OSINT")
-root.geometry("100x100")
+root.geometry("1920x1080")
 
 text_area = Text(root,height=20,width=150)
 text_area.pack()
@@ -30,31 +30,40 @@ def on_leave(e):
     e.widget.configure(style="TButton")    
 
 def ip_lookup_main():
-    ip = simpledialog.askstring("ip", "Enter an IP adresse to scan : ")
+    ip = simpledialog.askstring("Ip Input", "Enter an IP adresse to scan : ")
     if not ip:
-        text_area.insert("Please, enter an appropriate IP adress.")
+        text_area.insert(END,"Please, enter an appropriate IP adress. \n")
         return
     ip_lookup(ip)
 
 def Subdomains_Scanner_main():
-    Subdomain = simpledialog.askstring("Subdomain","Enter a domain's name to scan : ")
+    Subdomain = simpledialog.askstring("Subdomain input","Enter a domain's name to scan : ")
     if not Subdomain:
-        text_area.insert(END,"Please, enter an appropriate domain's name.")
+        text_area.insert(END,"Please, enter an appropriate domain's name. \n")
         return
-    Words_Length = simpledialog.askinteger("Word Length", "Enter the Word range you want to test in the word list (Max 114441) : ")
+    Words_Length = simpledialog.askinteger("Word Length input", "Enter the Word range you want to test in the word list (Max 114441) : ")
     if not Words_Length:
         text_area.insert(END,"Please enter a correct value : ")
         return
     Subdomains_Scanner(Subdomain)
     
 def Whois_Lookup_main():
-    Domain = simpledialog.askstring("Enter a domain's name : ")
+    Domain = simpledialog.askstring("Domain input","Enter a domain's name : ")
     if not Domain:
-        text_area.insert(END,"Enter an appropriate domain's name.")
+        text_area.insert(END,"Please enter an appropriate domain's name. \n")
         return
     get_whois_info(Domain)
+
+def email_finder_scan():
+    Domain = simpledialog.askstring("Domain input","Enter a domain's name : ")
+    if not Domain:
+        text_area.insert(END,"Enter a valid domain name. \n ")
+        return
+    email_finder(Domain)
     
+    #faire le bouton pour cette fonction
     
+
     
 Button_IP = ttk.Button(root,text="IP Lookup",command=ip_lookup_main, style="TButton")
 Button_IP.bind("<Enter>", on_enter)
@@ -70,5 +79,12 @@ Button_Whois = ttk.Button(root,text="Whois Lookup",  command=Whois_Lookup_main, 
 Button_Whois.bind("<Enter>", on_enter)
 Button_Whois.bind("<Leave>", on_leave)
 Button_Whois.pack()    
+
     
+Button_Email = ttk.Button(root,text="Email finder", command=email_finder_scan, style="TButton")
+Button_Email.bind("<Enter>", on_enter)
+Button_Email.bind("<Leave>", on_leave)
+Button_Email.pack()
+
+
 root.mainloop()
